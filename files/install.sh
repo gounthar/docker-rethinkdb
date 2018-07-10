acc5b6c90f31f44a91e01558cfc82a50f21c6ced
Bruno Verachten <116569+gounthar@users.noreply.github.com>
2018-07-10 18:16:42 +0200

    Update install.sh

--- files/install.sh
+++ files/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apk update 
 apk add wget 
-source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | tee /etc/apt/sources.list.d/rethinkdb.list
+source /etc/alpine-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | tee /etc/apt/sources.list.d/rethinkdb.list
 wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | apt-key add -
 apk update
 apk add rethinkdb
