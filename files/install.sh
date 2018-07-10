9b744a2eba1162ab26c38136aa4bf642635abda0
Bruno Verachten <116569+gounthar@users.noreply.github.com>
2018-07-10 18:11:12 +0200

    Update install.sh

--- files/install.sh
+++ files/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-apt-get update -q
-apt-get install wget -qy
+apk update 
+apk add wget 
 source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | tee /etc/apt/sources.list.d/rethinkdb.list
 wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | apt-key add -
-apt-get update -q
-apt-get install rethinkdb -qy
+apk update
+apk add rethinkdb
